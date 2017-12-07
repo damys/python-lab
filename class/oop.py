7692e8480a10c7337d652b941f42b197f8405c93
damys <damys@foxmail.com>
2017-12-07 09:10:30 +0800

    update

--- class/oop.py
+++ class/oop.py
@@ -4,3 +4,19 @@
 在级属性，方法命名时，实际是对名称做了一些特殊处理，使得外界无法访问到
 处理方式：在名称前面加上: 定义__类名， 调用：_类名__名称
 """
+
+class Women:
+    def __init__(self, name):
+        self.name = name
+        self.__age = 18  # 私有
+ 
+    def __secret(self):
+        return  self.__age
+ 
+ 
+if __name__ == '__main__':
+    rose = Women('rose')
+ 
+    # 伪属性，方法
+    print(rose._Women__age)       # 18
+    print(rose._Women__secret())  # 18
