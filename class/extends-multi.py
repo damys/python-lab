19410fdaeef96ac42aec856fa943922f73deba71
damys <damys@foxmail.com>
2017-12-12 16:07:42 +0800

    add extends multi

---
+++ class/extends-multi.py
@@ -0,0 +1,25 @@
+class A:
+     
+    def a(self):
+        print('a...')
+ 
+class B:
+ 
+    def b(self):
+        print("b...")
+ 
+ 
+class C(A, B):
+ 
+    def c(self):
+        print("c...")
+ 
+c = C()
+c.a()
+c.b()
+c.c()
+ 
+ 
+# 从左到右顺序方法搜索, 搜索不到会报错
+print(C.__mro__)
+# (<class '__main__.C'$gt;, $lt;class '__main__.A'$gt;, $lt;class '__main__.B'$gt;, $lt;class 'object'$gt;)
