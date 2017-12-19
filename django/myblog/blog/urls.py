c163b9bbc34ee30d6d4f9489f2c83b2b4999bb00
damys <damys@foxmail.com>
2017-12-19 14:11:58 +0800

    update

--- django/myblog/blog/urls.py
+++ django/myblog/blog/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # url(r'^', views.index)       # （正则）所有地址
-    url(r'^index/', views.index)
+    # url(r'^', views.blog),       # （正则）所有地址
+    url(r'^index/$', views.index),
+    # url(r'^article/(?P<article_id>[0-9]+)/$', views.article_page),
+    url(r'^article/(?P<article_id>[0-9]+)/$', views.article_page, name='article_page')
 ]
--- django/myblog/blog/views.py
+++ django/myblog/blog/views.py
@@ -7,8 +7,17 @@ from . import models
 def index(request):
     # return HttpResponse("hello Django")
 
-    # return render(request, 'index/index.html' )
+    # return render(request, 'blog/blog.html' )
 
-    article = models.Article.objects.get(pk=1)
+    # 获取一条
+    # article = models.Article.objects.get(pk=1)
+    # return render(request, 'blog/index.html', {'article': article})
 
-    return render(request, 'index/index.html', {'article':article})
+    # 获取所有
+    articles = models.Article.objects.all()
+    return render(request, 'blog/index.html', {'articles':articles})
+
+
+def article_page(request, article_id):
+    article = models.Article.objects.get(pk=article_id)
+    return render(request, 'blog/article_page.html', {'article': article})
