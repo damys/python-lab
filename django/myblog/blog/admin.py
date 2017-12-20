ce6462961b037d6543cc7f9a8d4a67b6b6affb42
damys <damys@foxmail.com>
2017-12-20 09:33:32 +0800

    update myblog db

--- django/myblog/blog/admin.py
+++ django/myblog/blog/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 
 from .models import Article
 
-admin.site.register(Article)
+# 默认显示
+# admin.site.register(Article)
+
+# 默认其它字段
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'pub_time')
+
+    # 添加时间过滤器
+    list_filter = ('pub_time', )
+
+admin.site.register(Article, ArticleAdmin)
+
+
